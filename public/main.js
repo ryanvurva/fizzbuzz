eb9320bf66b70d7e150ff8764b69419799feb2a6
Ryan Vurva <ryanvurva@gmail.com>
2017-03-20 15:19:23 -0400

    figured out the code to use, now reading and writing

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+for (var i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log('FizzBuzz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else {
+    console.log(i)
+  }
 }
-
-document.addEventListener('DOMContentLoaded', main)
-
